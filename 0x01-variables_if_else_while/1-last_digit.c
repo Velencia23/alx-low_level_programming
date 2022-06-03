da6ea2a5f2c22097a67888e4f806c8b3816ba769
Velencia23 <velenciagovender@ymail.com>
2022-06-03 11:38:43 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Prints variables
  *
@@ -7,8 +8,9 @@
 int main(void)
 {
 char a = 'a' ;
-while (a < = 'z')
+while (a <= 'z')
 putchar(a);
+a++;
 putchar('\n');
 return (0);
 }
