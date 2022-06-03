8db198997b06b3ecbb58ef10e0ca0640914466ef
Velencia23 <velenciagovender@ymail.com>
2022-06-03 12:08:22 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - Entry point
+ * main - Print entry point
  *
- * Return: 0 Success
+ * Return: 0
  */
 int main(void)
 {
-char abc;
-for (abc = 'a' ; abc <= 'z' ; abc++)
-{
-putchar(abc);
-}
-putchar('\n');
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
 return (0);
 }
+
