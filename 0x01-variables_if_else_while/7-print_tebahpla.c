e746a489fe7ffcc66d012276b0b5a9c135026f0e
Velencia23 <velenciagovender@ymail.com>
2022-06-03 14:37:46 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char abc;
-for (abc = 'z'; abc >= 'a' abc--)
+char letter; /*Decarling statement*/
+/*for z-a*/
+for (letter = 122 ; letter >= 97; letter--)
 {
-putchar(abc);
+putchar(letter);
 }
-putchar('\n');
+putchar('\n'); /*new line*/
 return (0);
 }
