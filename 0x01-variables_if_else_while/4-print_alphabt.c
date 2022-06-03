8f36981f07507a0833b0c66b8bd21c195212887e
Velencia23 <velenciagovender@ymail.com>
2022-06-03 12:38:12 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 char ch = 'a';
 while (ch <= 'z')
 {
-  if (ch != 'e' && ch != 'q')
+if (ch == 'e' || ch == 'q')
 {
+continue;
+} 
 putchar(ch)
-}
-ch++;
 }
 putchar('\n');
 return (0);
