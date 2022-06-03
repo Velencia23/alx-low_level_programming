825d870ffb3160e821b73871ba4bae03c24fb75d
Velencia23 <velenciagovender@ymail.com>
2022-06-03 14:29:34 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 /**
- * main - prints all the numbers of base 16 in lowercase
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 48; i < 58; i++)
-putchar(i);
-for (i = 'a'; i <= 'f'; i++)
-putchar(i);
+char base_16; /*Decarling statement*/
+/*for base 16 0-9*/
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16); /*print 0-9*/
 }
-putchar('\n');
+/*for base 16 a-f*/
+for (base_16 = 97 ; base_16 <= 102; base_16++)
+{
+putchar(base_16); /*print a-f*/
+}
+putchar ('\n'); /*print new line*/
 return (0);
 }
