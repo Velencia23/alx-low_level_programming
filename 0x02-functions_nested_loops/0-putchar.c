da3a49ba8858d5f56c30efb3d27fddc6de73dc3e
Velencia23 <velenciagovender@ymail.com>
2022-06-08 14:07:16 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: nothing, return void
  */
 int main(void)
 {
- _putchar('H');
- _putchar('o');
- _putchar('l');
- _putchar('b');
- _putchar('e');
- _putchar('r');
- _putchar('t');
- _putchar('o');
- _putchar('n');
- _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
  return (0);
 }
